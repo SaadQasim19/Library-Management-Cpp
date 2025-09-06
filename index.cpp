f9fcbfb81abf86a775e53df4d8e5399c4a7bf0e0
SaadQasim19 <m.saadqasim7665@gmail.com>
2025-09-06 23:38:05 +0500

    Code : Book Clas

--- index.cpp
+++ index.cpp
@@ -3,6 +3,35 @@
 #include <fstream>
 #include <limits>
 
+using namespace std;
+
+class Book {
+public:
+    int id;
+    string name;
+    string author;
+    bool isIssued;
+    string issuedTo;
+
+    Book(int bookId, string bookName, string bookAuthor) {
+        id = bookId;
+        name = bookName;
+        author = bookAuthor;
+        isIssued = false;
+        issuedTo = "";
+    }
+
+    void display() {
+        cout << "ID: " << id << " | Book: " << name << " | Author: " << author;
+        if (isIssued) {
+            cout << " | [ISSUED to: " << issuedTo << "]";
+        } else {
+            cout << " | [AVAILABLE]";
+        }
+        cout << endl;
+    }
+};
+
 int main(){
 
 }
